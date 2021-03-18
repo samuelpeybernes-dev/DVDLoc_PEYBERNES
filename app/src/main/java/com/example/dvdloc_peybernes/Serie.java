1e21a084d40f9ef942feb15b79c453d8f04b5b5d
Samuel <samuelpeybernes33@gmail.com>
2021-03-19 00:32:12 +0100

    TP6 b - page 3

--- app/src/main/java/com/example/dvdloc_peybernes/Serie.java
+++ app/src/main/java/com/example/dvdloc_peybernes/Serie.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Serie extends AppCompatActivity {
 
@@ -13,6 +20,55 @@ public class Serie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_serie);
+
+        //Tableau de séries
+        ArrayList<Serie> lesSeries = new ArrayList<Serie>();
+
+        //ListView listeSeries = (ListView) findViewById(R.id.serie_listview);
+        //SerieAdapter adapterSerie = new SerieAdapter(this, R.layout.ligne);
+
+        try{
+            XmlPullParser xmlPullParser=getResources().getXml(R.xml.liste_serie);
+            //Tant que la fin de fichier non atteinte
+            while (xmlPullParser.getEventType()!=XmlPullParser.END_DOCUMENT){
+                //Si balise ouvrante
+                if (xmlPullParser.getEventType()==xmlPullParser.START_TAG){
+                    //Si la balise est DVD
+                    if (xmlPullParser.getName().equals("dvd")){
+                        Serie uneSerie = new Serie();
+                        uneSerie.setCat(xmlPullParser.getAttributeValue(0));
+                        Log.i("LocDVD", "Cat = " + uneSerie.getCat());
+                        uneSerie.setTitre(xmlPullParser.getAttributeValue(1));
+                        Log.i("LocDVD", "Titre = " + uneSerie.getTitre());
+                        uneSerie.setRealisateur(xmlPullParser.getAttributeValue(2));
+                        Log.i("LocDVD", "Réalisateur = " + uneSerie.getRealisateur());
+
+                        //Récupération du nom de l'image dans drawable
+                        String path = getOpPackageName() + ":drawable/"
+                                         + xmlPullParser.getAttributeValue(3);
+                        int resID = getResources().getIdentifier(path, null, null);
+                        uneSerie.setImg(resID);
+                        Log.i("LocDVD", "Img = " + uneSerie.getImg());
+
+                        //Ajout de l'objet
+                        lesSeries.add(uneSerie);
+                        //Alimentation de l'adapter
+                        //adapterSerie.add(uneSerie);
+                    }
+                }
+                //Enregistrement suivant
+                xmlPullParser.next();
+            }
+        }
+        catch (Exception e){
+            Log.i("LocDVD", "Erreurs trouvées = " + e.getMessage());
+            e.printStackTrace();
+        }
+        listeSeries.setAdapter(adapterSerie);
+
+
+
+
 
         //Gestion du bouton btnRetour
         final Button btnRetourSerie = (Button) findViewById(R.id.btnRetourSerie);
---
+++ app/src/main/java/data/locdvd/Serie.java
@@ -0,0 +1,24 @@
+package data.locdvd;
+
+public class Serie {
+    private String strCat = "";
+    private String strTitre = "";
+    private String strRealisateur = "";
+    private int iImg = 0;
+
+    //Constructeur par défaut
+    public Serie(){}
+
+    //Mutateurs
+    public void setCat(String cat) {this.strCat = cat;}
+    public void setTitre(String titre) {this.strTitre = titre;}
+    public void setRealisateur(String realisateur) {this.strRealisateur = realisateur;}
+    public void setImg(int img) {this.iImg = img;}
+
+    //Accesseurs
+    public String getCat(){return this.strCat;}
+    public String getTitre(){return this.strTitre;}
+    public String getRealisateur(){return this.strRealisateur;}
+    public int getImg(){return this.iImg;}
+
+}
