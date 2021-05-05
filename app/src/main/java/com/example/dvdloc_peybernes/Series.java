682337c2523046684045273f6df2c01eeac268f8
Samuel <samuelpeybernes33@gmail.com>
2021-05-05 18:58:20 +0200

    TP 7 finis

--- app/src/main/java/com/example/dvdloc_peybernes/Series.java
+++ app/src/main/java/com/example/dvdloc_peybernes/Series.java
@@ -10,6 +10,13 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -21,7 +28,7 @@ import data.locdvd.Serie;
 import data.locdvd.SerieAdapter;
 
 public class Series extends AppCompatActivity {
-    String url = "http://192.168.1.2/volley_serv/listeDvd.php";
+    String url = "http://192.168.1.21/volley_serv/listeDvd.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,7 +39,7 @@ public class Series extends AppCompatActivity {
 
         //Variables privée pour la listview et le videoAdapter
         ListView listeSeries = (ListView) findViewById(R.id.serie_listview);
-        final VideoAdapter adapterVideo = new VideoAdapter (this, R.layout.ligne);
+        final SerieAdapter adapterVideo = new SerieAdapter (this, R.layout.ligne);
 
         final RequestQueue requestQueue = Volley.newRequestQueue (getApplicationContext());
         JsonArrayRequest arrayRequest = new JsonArrayRequest (
@@ -46,15 +53,15 @@ public class Series extends AppCompatActivity {
                         while (count < response.length()){
                             try{
                                 JSONObject jsonObject = response.getJSONObject(count);
-                                Video uneVideo = new Video();
-                                uneVideo.setStrTitre (jsonObject.getString("titreSerie"));
-                                uneVideo.setStrRealisateur (jsonObject.getString("realisateur"));
+                                Serie  uneSerie = new Serie();
+                                uneSerie.setTitre (jsonObject.getString("titreSerie"));
+                                uneSerie.setRealisateur (jsonObject.getString("realisateur"));
                                 //récupération nom de l'image dans Drawable
                                 String path = getPackageName()+ ":drawable/" + jsonObject.getString("image");
                                 int resID = getResources().getIdentifier(path, null, null);
-                                uneVideo.setImg(resID);
+                                uneSerie.setImg(resID);
                                 //Ajout de l'objet
-                                adapterVideo.add(uneVideo);
+                                adapterVideo.add(uneSerie);
                                 count++;
                             } catch (JSONException e){
                                 e.printStackTrace();
@@ -75,53 +82,53 @@ public class Series extends AppCompatActivity {
 
 
         //--------------------------------------------------------------------------
-
+/*
         //Tableau de séries
-        //ArrayList<Serie> lesSeries = new ArrayList<Serie>();
+        ArrayList<Serie> lesSeries = new ArrayList<Serie>();
 
-        //ListView listeSeries = (ListView) findViewById(R.id.serie_listview);
-        //SerieAdapter adapterSerie = new SerieAdapter(this, R.layout.ligne);
+        ListView listeSeries = (ListView) findViewById(R.id.serie_listview);
+        SerieAdapter adapterSerie = new SerieAdapter(this, R.layout.ligne);
 
 
-        //try{
-        //    XmlPullParser xmlPullParser=getResources().getXml(R.xml.liste_serie);
-        //    //Tant que la fin de fichier non atteinte
-        //   while (xmlPullParser.getEventType()!=XmlPullParser.END_DOCUMENT){
-        //        //Si balise ouvrante
-        //       if (xmlPullParser.getEventType()==xmlPullParser.START_TAG){
-        //           //Si la balise est DVD
-        //           if (xmlPullParser.getName().equals("dvd")){
-        //               Serie uneSerie = new Serie();
-        //               uneSerie.setCat(xmlPullParser.getAttributeValue(0));
-        //               //Log.i("LocDVD", "Cat = " + uneSerie.getCat());
-        //               uneSerie.setTitre(xmlPullParser.getAttributeValue(1));
-        //               //Log.i("LocDVD", "Titre = " + uneSerie.getTitre());
-        //               uneSerie.setRealisateur(xmlPullParser.getAttributeValue(2));
-        //               //Log.i("LocDVD", "Réalisateur = " + uneSerie.getRealisateur());
+        try{
+            XmlPullParser xmlPullParser=getResources().getXml(R.xml.liste_serie);
+            //Tant que la fin de fichier non atteinte
+           while (xmlPullParser.getEventType()!=XmlPullParser.END_DOCUMENT){
+                //Si balise ouvrante
+               if (xmlPullParser.getEventType()==xmlPullParser.START_TAG){
+                   //Si la balise est DVD
+                   if (xmlPullParser.getName().equals("dvd")){
+                       Serie uneSerie = new Serie();
+                       uneSerie.setCat(xmlPullParser.getAttributeValue(0));
+                       //Log.i("LocDVD", "Cat = " + uneSerie.getCat());
+                       uneSerie.setTitre(xmlPullParser.getAttributeValue(1));
+                       //Log.i("LocDVD", "Titre = " + uneSerie.getTitre());
+                       uneSerie.setRealisateur(xmlPullParser.getAttributeValue(2));
+                       //Log.i("LocDVD", "Réalisateur = " + uneSerie.getRealisateur());
 
-        //               //Récupération du nom de l'image dans drawable
-        //               String path = getPackageName() + ":drawable/"
-        //                                + xmlPullParser.getAttributeValue(3);
-        //               int resID = getResources().getIdentifier(path, null, null);
-        //               uneSerie.setImg(resID);
+                      //Récupération du nom de l'image dans drawable
+                       String path = getPackageName() + ":drawable/"
+                                        + xmlPullParser.getAttributeValue(3);
+                       int resID = getResources().getIdentifier(path, null, null);
+                       uneSerie.setImg(resID);
                       //Log.i("LocDVD", "Img = " + uneSerie.getImg());
 
                          //Ajout de l'objet
-        //               lesSeries.add(uneSerie);
+                       lesSeries.add(uneSerie);
                          //Alimentation de l'adapter
-        //               adapterSerie.add(uneSerie);
-        //           }
-        //       }
+                       adapterSerie.add(uneSerie);
+                   }
+               }
                 //Enregistrement suivant
-        //       xmlPullParser.next();
-        //   }
-        // }
-        //  catch (Exception e){
+               xmlPullParser.next();
+           }
+         }
+          catch (Exception e){
             //Log.i("LocDVD", "Erreurs trouvées = " + e.getMessage());
-        //   e.printStackTrace();
-        // }
-        //  listeSeries.setAdapter(adapterSerie);
-
+           e.printStackTrace();
+         }
+          listeSeries.setAdapter(adapterSerie);
+*/
 
         //--------------------------------------------------------------------------
 
