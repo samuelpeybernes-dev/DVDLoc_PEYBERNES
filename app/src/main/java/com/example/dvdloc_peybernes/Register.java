df1862310a4eab748c33782641d22d7b75da20fa
Samuel <samuelpeybernes33@gmail.com>
2021-05-24 17:53:23 +0200

    TP 8 fin?

--- app/src/main/java/com/example/dvdloc_peybernes/Register.java
+++ app/src/main/java/com/example/dvdloc_peybernes/Register.java
@@ -1,15 +1,14 @@
 package com.example.dvdloc_peybernes;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -27,21 +26,20 @@ import java.util.Map;
 
 import dao.C;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
+
     EditText edt_Email;
     EditText edt_paswd;
     Button btn_ok;
-    TextView txt_inscrire;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         edt_Email = (EditText)findViewById(R.id.edt_email);
         edt_paswd = (EditText)findViewById(R.id.edt_pswd);
         btn_ok = (Button)findViewById(R.id.btn_ok_register);
-        txt_inscrire = (TextView) findViewById(R.id.txt_inscrire);
 
         btn_ok.setOnClickListener(new Button.OnClickListener()
         {
@@ -53,57 +51,43 @@ public class Login extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Veuillez saisir les deux valeurs !", Toast.LENGTH_SHORT).show();
                 }
                 else{
-                    loginUser();
+                    registerUser();
+                    Intent intent = new Intent(Register.this, Login.class);
+                    intent.putExtra("titre", edt_Email.getText().toString());
+                    startActivity(intent);
                 }
-            }
-        });
-
-        txt_inscrire.setOnClickListener(new TextView.OnClickListener()
-        {
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(Login.this, Register.class);
-                startActivity(intent);
             }
         });
     }
 
-    public void loginUser(){
+    public void registerUser(){
         final String email = edt_Email.getText().toString().trim();
         final String pswd = edt_paswd.getText().toString().trim();
-
         StringRequest stringrequest = new StringRequest(Request.Method.POST,
-                C.Login_URL,
-                new Response.Listener<String>() {
+                C.Register_URL,
+                new Response.Listener<String>(){
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(String response){
                         try {
                             JSONObject jsonObject = new JSONObject(response);
-                            Log.i("Test connexion", response);
+                            Log.i("Test ajout", response);
                             Toast.makeText(getApplicationContext(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
-                            if(jsonObject.getString("error") == "false"){
-                                Intent intent = new Intent(Login.this, MainActivity.class);
-                                intent.putExtra("titre", edt_Email.getText().toString());
-                                startActivity(intent);
-                            }
                         } catch (JSONException e){
                             e.printStackTrace();
                         }
                     }
                 },
-                new Response.ErrorListener() {
+                new Response.ErrorListener(){
                     @Override
-                    public void onErrorResponse(VolleyError erreur) {
+                    public void onErrorResponse(VolleyError erreur){
                         Toast.makeText(getApplicationContext(), erreur.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 }){
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String,String> params = new HashMap<>();
-                if (!email.equals("")&&!pswd.equals("")){
-                    params.put("email", email);
-                    params.put("password", pswd);
-                }
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", pswd);
                 return params;
             }
         };
---
+++ app/src/main/java/dao/C.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class C {
+    public static final String ROOT_URL="http://192.168.1.14/locDvd";
+    public static final String Register_URL=ROOT_URL+"/server/userRegister.php";
+    public static final String Login_URL=ROOT_URL+"/server/userLogin.php";
+}
